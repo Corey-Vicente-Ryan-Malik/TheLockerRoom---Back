07224f2f563b58c33cf75ee518f743b8ca3a8128
Vicente Figueroa <figueroa.vicente9@gmail.com>
2022-12-05 17:16:37 -0600

    registered users are now stored in database and users can now be deleted

--- src/main/java/com/capstone/lockerapi/controllers/UserController.java
+++ src/main/java/com/capstone/lockerapi/controllers/UserController.java
@@ -1,4 +1,28 @@
 package com.capstone.lockerapi.controllers;
 
+
+import com.capstone.lockerapi.models.User;
+import com.capstone.lockerapi.repositories.UserRepository;
+import com.capstone.lockerapi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteUser(@PathVariable long id) {
+        userService.deleteUserById(id);
+        return "User deleted.";
+    }
+
 }
--- src/main/java/com/capstone/lockerapi/services/UserServiceImpl.java
+++ src/main/java/com/capstone/lockerapi/services/UserServiceImpl.java
@@ -1,4 +1,28 @@
 package com.capstone.lockerapi.services;
 
-public class UserServiceImpl {
+import com.capstone.lockerapi.models.User;
+import com.capstone.lockerapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findUserById(long id) {
+        return userRepository.getReferenceById(id);
+    }
+
+    @Override
+    public void deleteUserById(long id) {
+        userRepository.deleteById(id);
+    }
 }
