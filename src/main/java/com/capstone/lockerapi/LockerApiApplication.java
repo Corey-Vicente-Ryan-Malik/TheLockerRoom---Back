095e2e07d8901e3b36982ee6e23dad4ebea1145f
Malik <willis.malik.r@gmail.com>
2023-01-04 14:40:38 -0600

    cors mapping

--- src/main/java/com/capstone/lockerapi/LockerApiApplication.java
+++ src/main/java/com/capstone/lockerapi/LockerApiApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LockerApiApplication {
@@ -13,4 +15,19 @@ public class LockerApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(LockerApiApplication.class, args);
     }
+    public WebMvcConfigurer corsMappingConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                //WebConfigProperties.Cors cors = webConfigProperties.getCors();
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .maxAge(3600)
+                        .allowedHeaders("*")
+                        .exposedHeaders("*");
+            }
+        };
+    }
+
 }
